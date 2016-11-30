a0b8403ab5ef590b278498d21cddc3106cca3878
Di Zhang <zhangdi_me@163.com>
2016-11-30 12:40:49 +0800

    Update Nav.php

--- src/Nav.php
+++ src/Nav.php
@@ -11,48 +11,48 @@ use yii\helpers\ArrayHelper;
 
 class Nav extends \yii\bootstrap\Nav
 {
-    public $itemsId;
+    public $nav;
 
     public function init()
     {
         parent::init();
-        if (isset($this->itemsId)) {
-            $this->items = self::getNavItems($this->itemsId);
+        if (isset($this->nav)) {
+            $this->items = self::getNavItems($this->nav);
         }
     }
 
     /**
      * 设置菜单
      *
-     * @param string $itemsId 菜单编号
-     * @param array $menuItems 菜单项
+     * @param string $nav 菜单编号
+     * @param array $items 菜单项
      */
-    public static function setNavItems($itemsId, array $menuItems = [])
+    public static function setNavItems($nav, array $items = [])
     {
-        \Yii::$app->getView()->params['menuItems'][$itemsId] = $menuItems;
+        \Yii::$app->getView()->params['navItems'][$nav] = $items;
     }
 
     /**
      * 获取菜单
      *
-     * @param string $menuId
+     * @param string $nav
      * @return array
      */
-    public static function getNavItems($menuId)
+    public static function getNavItems($nav)
     {
-        return ArrayHelper::getValue(\Yii::$app->getView()->params, "menuItems.{$menuId}", []);
+        return ArrayHelper::getValue(\Yii::$app->getView()->params, "navItems.{$nav}", []);
     }
 
     /**
      * 添加菜单项
      *
-     * @param string $itemsId 菜单编号
+     * @param string $nav 菜单编号
      * @param string|array $item 菜单项
      */
-    public static function addMenuItem($itemsId, $item)
+    public static function addMenuItem($nav, $item)
     {
-        $menu = static::getNavItems($itemsId);
-        $menu[] = $item;
-        \Yii::$app->getView()->params['menuItems'][$itemsId] = $menu;
+        $items = static::getNavItems($nav);
+        $items[] = $item;
+        \Yii::$app->getView()->params['navItems'][$nav] = $items;
     }
 }
